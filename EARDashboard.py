030d40d29cf87886a7b5fd44d2f802b58f265bbd
felipesouzabd <felipesbd89@gmail.com>
2024-05-10 13:44:06 -0300

    Update EARDashboard.py

--- EARDashboard.py
+++ EARDashboard.py
@@ -5,10 +5,11 @@ Created on Thu May  9 15:52:22 2024
 @author: felipe.damasceno
 """
 
-import pandas as pd
-import numpy as np
 import streamlit as st
 
-df = pd.read_excel('EAR_DIARIO_SUBSISTEMA_2022.xlsx', sheet_name='Sheet')
+streamlit.title('hello, world')
+
+streamlit.header('header')
+streamlit.text('text1')
 
 
